3f244b0e938959209060bc04a23cd3a840e19668
yazeedahmed <dee3ayahmed25@gmail.com>
2023-03-22 14:04:02 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const spy = ['Guadalupe','Ollie', 'Aki'];
+function  writeCards(spy){
+    const surprise = []
+    for(let i=0; i < spy.length ;  i++){
+        surprise.push (`Thank you, ${spy[i]}, for the wonderful surprise gift!`)
+        console.log( surprise);
+    }
+    return surprise;
+}
+function countDown(){
+    for (let j = 10; j>=0; j--){
+        console.log(j)
+    }
+}
